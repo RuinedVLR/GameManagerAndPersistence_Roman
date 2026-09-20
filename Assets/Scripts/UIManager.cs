9d933ad175852d8e2fc3ac8154bbab047a1b4963
RuinedVLR <rzhurakhov@gmail.com>
2026-09-20 13:42:18 -0300

    UI navigation in main menu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Canvas References")]
+    [SerializeField] GameObject _mainMenuCanvas;
+    [SerializeField] GameObject _levelSelectCanvas;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        OpenMainMenu();
+    }
+
+    void CloseAllUI()
+    {
+        _mainMenuCanvas.SetActive(false);
+        _levelSelectCanvas.SetActive(false);
+    }
+
+    public void OpenMainMenu()
+    {
+        CloseAllUI();
+        _mainMenuCanvas.SetActive(true);
+    }
+
+    public void OpenLevelSelect()
+    {
+        CloseAllUI();
+        _levelSelectCanvas.SetActive(true);
+    }
+}
